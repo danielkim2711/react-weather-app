89ffb7075805592620d75cc480128ef3b790c1b2
Daniel Kim <danielkim2711@gmail.com>
2022-08-08 19:20:39 +1200

    Style CurrentWeather component for searching

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,34 +1,59 @@
 import landingImage from '../assets/images/landing.png';
 
 const CurrentWeather = ({ currentWeatherData }) => {
+  const [weatherDescription1, weatherDescription2] = currentWeatherData
+    ? currentWeatherData.weather[0].description.split(' ')
+    : '';
+
   return (
     <>
       {currentWeatherData ? (
         <>
-          <div className='flex--column'>
-            <h1>
-              {currentWeatherData.name}, {currentWeatherData.sys.country}
-            </h1>
+          <h1 className='weather__city'>
+            {currentWeatherData.name}, {currentWeatherData.sys.country}
+          </h1>
+          <div className='weather__container flex--column'>
             <img
               src={`icons/${currentWeatherData.weather[0].icon}.png`}
               alt='weather'
-              className='weather-icon'
+              className='weather__icon'
             />
-            <p>{Math.round(currentWeatherData.main.temp)}°C</p>
-            <p>{currentWeatherData.weather[0].description}</p>
-            <p>Feels like {Math.round(currentWeatherData.main.feels_like)}°C</p>
-            <p>Wind: {currentWeatherData.wind.speed} m/s</p>
-            <p>Humidity: {currentWeatherData.main.humidity}%</p>
+            <p className='weather__description'>
+              {`${
+                weatherDescription1.charAt(0).toUpperCase() +
+                weatherDescription1.slice(1)
+              } ${
+                weatherDescription2.charAt(0).toUpperCase() +
+                weatherDescription2.slice(1)
+              }`}
+            </p>
+            <p className='weather__temperature'>
+              {Math.round(currentWeatherData.main.temp)}°C
+            </p>
+            <div className='weather__info__container'>
+              <div>
+                <p>Feels like</p>
+                <span>{Math.round(currentWeatherData.main.feels_like)}°C</span>
+              </div>
+              <div>
+                <p>Wind:</p>
+                <span>{currentWeatherData.wind.speed} m/s</span>
+              </div>
+              <div>
+                <p>Humidity: </p>
+                <span>{currentWeatherData.main.humidity}%</span>
+              </div>
+            </div>
           </div>
         </>
       ) : (
-        <div className='flex--column'>
+        <div className='landing__container flex--column'>
           <img src={landingImage} alt='landing' className='landing__image' />
           <h1>
             Weather <span className='landing__title'>App</span>
           </h1>
           <p className='landing__description'>
-            Find weather for cities at your fingertips!
+            Find current weather for cities at your fingertips!
           </p>
         </div>
       )}
